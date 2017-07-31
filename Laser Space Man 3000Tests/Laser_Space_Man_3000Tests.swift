57a30028f03470d0c69d46d90a0e7528bd3c071f
Josh Zignego <ziggybadger@gmail.com>
2017-07-31 17:35:38 -0500

    Initial Commit

---
+++ Laser Space Man 3000Tests/Laser_Space_Man_3000Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Laser_Space_Man_3000Tests.swift
+//  Laser Space Man 3000Tests
+//
+//  Created by Josh Zignego on 7/31/17.
+//  Copyright © 2017 Josh Zignego. All rights reserved.
+//
+
+import XCTest
+@testable import Laser_Space_Man_3000
+
+class Laser_Space_Man_3000Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
