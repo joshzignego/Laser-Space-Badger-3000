3e78b54b9832d06c88cc25e81af153f09edcf2b4
Josh Zignego <ziggybadger@gmail.com>
2017-08-02 02:35:55 -0500

    Ending 8/1

--- Laser Space Man 3000/GameOverScene.swift
+++ Laser Space Man 3000/GameOverScene.swift
@@ -11,40 +11,11 @@ import SpriteKit
 
 class GameOverScene: SKScene {
     
-    var playAgainButton = SKSpriteNode()
-    let playAgainButtonText = SKTexture(imageNamed: "PlayAgainButton")
-    var mainMenuButton = SKSpriteNode()
-    let mainMenuButtonText = SKTexture(imageNamed: "MainMenuButton")
+    var playAgainButton = SKShapeNode()
+    var mainMenuButton = SKShapeNode()
     
     override init(size: CGSize) {
-        
         super.init(size: size)
-        
-        // 1
-        backgroundColor = SKColor.white
-        
-        // 2
-        let message = "Game Over"
-        
-        // 3
-        let label = SKLabelNode(fontNamed: "Helvetica-Bold")
-        label.text = message
-        label.fontSize = 40
-        label.fontColor = SKColor.black
-        label.position = CGPoint(x: size.width/2, y: size.height/2)
-        addChild(label)
-        
-        /*
-        run(SKAction.sequence([
-            SKAction.wait(forDuration: 3.0),
-            SKAction.run() {
-                // 5
-                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
-                let scene = GameScene(size: size)
-                self.view?.presentScene(scene, transition:reveal)
-            }
-            ]))
-        */
     }
     
     // 6
@@ -53,8 +24,46 @@ class GameOverScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
+        backgroundColor = SKColor.white
         
-        playAgainButton = SKSpriteNode(texture: playAgainButtonText)
+        
+        let path = CGRect.init(x: Double(size.width/4), y: Double(size.height*33/100), width: Double(size.width/2), height: Double(size.height/4))
+        playAgainButton = SKShapeNode.init(rect: path, cornerRadius: 10)
+        playAgainButton.strokeColor = UIColor.black
+        self.addChild(playAgainButton)
+
+        let path2 = CGRect.init(x: Double(size.width/4), y: Double(size.height*5/100), width: Double(size.width/2), height: Double(size.height/4))
+        mainMenuButton = SKShapeNode.init(rect: path2, cornerRadius: 10)
+        mainMenuButton.strokeColor = UIColor.black
+        self.addChild(mainMenuButton)
+        
+        let message = "Game Over"
+        let label = SKLabelNode(fontNamed: "Fipps-Regular")
+        label.text = message
+        label.fontSize = 60
+        label.fontColor = SKColor.blue
+        label.position = CGPoint(x: size.width/2, y: size.height*66/100)
+        self.addChild(label)
+        
+        let message2 = "Play Again"
+        let label2 = SKLabelNode(fontNamed: "Fipps-Regular")
+        label2.text = message2
+        label2.fontSize = 40
+        label2.fontColor = SKColor.blue
+        label2.position = CGPoint(x: size.width/2, y: size.height*41/100)
+        self.addChild(label2)
+        
+        let message3 = "Main Menu"
+        let label3 = SKLabelNode(fontNamed: "Fipps-Regular")
+        label3.text = message3
+        label3.fontSize = 40
+        label3.fontColor = SKColor.blue
+        label3.position = CGPoint(x: size.width/2, y: size.height*12/100)
+        self.addChild(label3)
+        
+        
+        
+      /*  playAgainButton = SKSpriteNode(texture: playAgainButtonText)
         playAgainButton.position = CGPoint(x: size.width/2, y: size.height*0.75)
         let width: Double = Double(size.width) * 0.25
         let height: Double = Double(size.height) * 0.25
@@ -64,7 +73,7 @@ class GameOverScene: SKScene {
         mainMenuButton = SKSpriteNode(texture: mainMenuButtonText)
         mainMenuButton.position = CGPoint(x: size.width/2, y: size.height*0.25)
         mainMenuButton.scale(to: CGSize(width: width, height: height))
-        self.addChild(mainMenuButton)
+        self.addChild(mainMenuButton)*/
         
     }
     
@@ -93,18 +102,3 @@ class GameOverScene: SKScene {
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
--- Laser Space Man 3000/MainMenuScene.swift
+++ Laser Space Man 3000/MainMenuScene.swift
@@ -11,17 +11,49 @@ import SpriteKit
 
 class MainMenuScene: SKScene {
     
-    var playButton = SKSpriteNode()
-    let playButtonText = SKTexture(imageNamed: "PlayButton")
+    //var playButton = SKSpriteNode()
+    var playButtonDoneAsAShape = SKShapeNode()
+    //let playButtonText = SKTexture(imageNamed: "PlayButton")
     
     override func didMove(to view: SKView) {
         
-        playButton = SKSpriteNode(texture: playButtonText)
-        playButton.position = CGPoint(x: frame.midX, y: frame.midY)
+        //playButton = SKSpriteNode(texture: playButtonText)
+        backgroundColor = SKColor.white
+        
+        // Try as a shape
+        let path = CGRect.init(x: Double(size.width/4), y: Double(size.height*1/4), width: Double(size.width/2), height: Double(size.height/4))
+        playButtonDoneAsAShape = SKShapeNode.init(rect: path, cornerRadius: 10)
+        playButtonDoneAsAShape.strokeColor = UIColor.black
+        self.addChild(playButtonDoneAsAShape)
+        
+        let message = "Play"
+        let label = SKLabelNode(fontNamed: "Fipps-Regular")
+        label.text = message
+        label.fontSize = 40
+        label.fontColor = SKColor.blue
+        label.position = CGPoint(x: size.width/2, y: size.height*32/100)
+        self.addChild(label)
+        
+        let titleMessage = "Laser Space Man 3000"
+        let title = SKLabelNode(fontNamed: "Fipps-Regular")
+        title.text = titleMessage
+        title.fontSize = 40
+        title.fontColor = SKColor.blue
+        title.position = CGPoint(x: size.width/2, y: size.height*66/100)
+        self.addChild(title)
+        
+        
+        //playButtonDoneAsAShape.position = CGPoint(x: frame.midX, y: frame.midY)
+        //let width: Double = Double(size.width) * 0.25
+        //let height: Double = Double(size.height) * 0.25
+        //playButtonDoneAsAShape.scale(to: CGSize(width: width, height: height))
+        //self.addChild(playButtonDoneAsAShape)
+        
+        /*playButton.position = CGPoint(x: frame.midX, y: frame.midY)
         let width: Double = Double(size.width) * 0.25
         let height: Double = Double(size.height) * 0.25
         playButton.scale(to: CGSize(width: width, height: height))
-        self.addChild(playButton)
+        self.addChild(playButton)*/
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -29,7 +61,7 @@ class MainMenuScene: SKScene {
             let pos = touch.location(in: self)
             let node = self.atPoint(pos)
             
-            if node == playButton {
+            if node == playButtonDoneAsAShape {
                 if let view = view {
                     let transition:SKTransition = SKTransition.fade(withDuration: 1)
                     let scene:SKScene = GameScene(size: self.size)
